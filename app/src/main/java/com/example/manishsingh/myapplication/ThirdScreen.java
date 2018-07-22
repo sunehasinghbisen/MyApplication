5a970e00dc6707ee8eb7d0f3c125030fb52a19cb
Manish Singh <manish.singh88@gmail.com>
2018-07-22 12:51:55 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/manishsingh/myapplication/ThirdScreen.java
@@ -0,0 +1,24 @@
+package com.example.manishsingh.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class ThirdScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_screen);
+        Button btn = (Button)findViewById(R.id.thirdScreenButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Clicked on third screen");
+
+
+            }
+        });
+    }
+}
